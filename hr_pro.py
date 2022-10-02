8042c04d387e801d4d574d26a6eb92b95de2a6b0
Engiineeer <ntpzlwo@gmail.com>
2022-10-02 20:13:13 +0300

    finished with bonus

--- hr_pro.py
+++ hr_pro.py
@@ -25,40 +25,40 @@ class Manager(Employee):
     def get_bonus(self,bonus_percentage):
         return bonus_percentage*self.salary
 
+n=1        
+while n>0:
+    employee_one = Employee("Laila",24,9999,4)
+    employee_two = Employee("Moh",27,999,2)
+    manager_one = Manager("sammy",52,4600,19,1380.0)
+    employee_list = [employee_one,employee_two]
+    manager_list = [manager_one]
+
+    print("Welcome to HR Pro\nOptions: \n   1. Show Employees\n   2. Show Managers\n   3. Add An Employee\n   4. Add A Manager\n   5. Exit\n\n")
+    the_input = int(input("What would you like to do? "))
+    if the_input == 1:
+        print("-----------------\nEmployees\n\n")
+        for emp in employee_list:
+            print(emp)
+
+    elif the_input == 2:
+        print("-----------------\Managers\n\n")
+        for mang in manager_list:
+            print(mang)
+
+    elif the_input == 3:
+        print("-----------------\n")
+        employee_list.append(Employee(input("name: "),int(input("Age: ")),int(input("Salary: ")),int(input("Employement years: "))))
         
+        print("Employee added succesfully\n")
+        for emp in employee_list:
+            print(emp)
+    elif the_input == 4:
+        print("-----------------\n")
+        manager_list.append(Manager(input("name: "),int(input("Age: ")),int(input("Salary: ")),int(input("Employement years: ")),float(input("Bonus Percentage: "))))
+        
+        print("Manager  added succesfully\n")
+        for mang in manager_list:
+            print(mang)
 
-employee_one = Employee("Laila",24,9999,4)
-employee_two = Employee("Moh",27,999,2)
-manager_one = Manager("sammy",52,4600,19,1380.0)
-employee_list = [employee_one,employee_two]
-manager_list = [manager_one]
-
-print("Welcome to HR Pro\nOptions: \n   1. Show Employees\n   2. Show Managers\n   3. Add An Employee\n   4. Add A Manager\n   5. Exit\n\n")
-the_input = int(input("What would you like to do? "))
-if the_input == 1:
-     print("-----------------\nEmployees\n\n")
-     for emp in employee_list:
-        print(emp)
-
-elif the_input == 2:
-    print("-----------------\Managers\n\n")
-    for mang in manager_list:
-        print(mang)
-
-elif the_input == 3:
-    print("-----------------\n")
-    employee_list.append(Employee(input("name: "),int(input("Age: ")),int(input("Salary: ")),int(input("Employement years: "))))
-    
-    print("Employee added succesfully\n")
-    for emp in employee_list:
-        print(emp)
-elif the_input == 4:
-    print("-----------------\n")
-    manager_list.append(Manager(input("name: "),int(input("Age: ")),int(input("Salary: ")),int(input("Employement years: ")),float(input("Bonus Percentage: "))))
-    
-    print("Manager  added succesfully\n")
-    for mang in manager_list:
-        print(mang)
-
-elif the_input == 5:
-    ...
+    elif the_input == 5:
+        break
